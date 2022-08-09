2ecd48643bff2b95fd4079153ae4239d25aa3452
Roman Khan Rajib <rajibkn345@gmail.com>
2022-08-09 14:55:50 +0600

    addign useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
@@ -7,6 +7,12 @@ import MainHeader from "./components/MainHeader/MainHeader";
 function App() {
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+	useEffect(() => {
+		const storedInformation = localStorage.getItem("isLoggedIn");
+		if (storedInformation === "1") {
+			setIsLoggedIn(true);
+		}
+	}, []);
 	const loginHandler = (email, password) => {
 		// We should of course check email and password
 		// But it's just a dummy/ demo anyways
@@ -16,6 +22,7 @@ function App() {
 	};
 
 	const logoutHandler = () => {
+		localStorage.removeItem("isLoggedIn");
 		setIsLoggedIn(false);
 	};
 
